3ef61b39a47f1965b39cbe5664c9f90f5135ac50
Felipe Madureira de Souza <felippefms3@gmail.com>
2021-10-21 11:40:11 -0300

    Update

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import './components/PhotoBox'
+import PhotoBox from './components/PhotoBox';
 
 function App() {
   return (
     <div className="App">
-
+      <PhotoBox></PhotoBox>
     </div>
   );
 }
---
+++ src/components/PhotoBox.jsx
@@ -0,0 +1,9 @@
+import './PhotoBox.css'
+
+ export default () => {
+     return (
+         <div className='PhotoBox'>
+             <p>imagem</p>
+         </div>
+     )
+ }
