f07e1eda805f328160b4e205990a528ba0f0e403
Felipe Madureira de Souza <Felippefms3@gmail.com>
2022-07-31 00:17:14 -0300

    update

--- src/components/Voadores.js
+++ src/components/Voadores.js
@@ -9,6 +9,11 @@ import beijaflor from '../imgs/beijaflor.jpg';
 import cegonha from '../imgs/cegonha.jpg';
 import gaivota from '../imgs/gaivota.jpg';
 import arara from '../imgs/arara.jpg';
+import tucano from '../imgs/tucano.jpg';
+import bluejay from '../imgs/bluejay.jpg';
+import coruja from '../imgs/coruja.jpg';
+import cardeal from '../imgs/cardinal.jpg';
+import flamingo from '../imgs/flamingo.jpg';
 
 function Voadores(){
     return(
@@ -19,13 +24,13 @@ function Voadores(){
             <PhotoBox imagem={cegonha} nome=' Cegonha' nomecien=' Ciconia' classe=' Ave' anos='30'></PhotoBox>
             <PhotoBox imagem={gaivota} nome=' Gaivota' nomecien=' Larus dominicanus' classe=' Ave' anos='30'></PhotoBox>
             <PhotoBox imagem={arara} nome=' Papagaio' nomecien=' Amazona aestiva' classe=' Ave' anos='60'></PhotoBox>
-            <PhotoBox></PhotoBox>
-            <PhotoBox></PhotoBox>
-            <PhotoBox></PhotoBox>
+            <PhotoBox imagem={tucano} nome=' Tucano' nomecien=' Ramphastidae' classe=' Ave' anos='20'></PhotoBox>
+            <PhotoBox imagem={bluejay} nome=' Gaio-Azul' nomecien=' Cyanocitta cristata' classe=' Ave' anos='7'></PhotoBox>
+            <PhotoBox imagem={coruja} nome=' Coruja' nomecien=' Strigiformes' classe=' Ave' anos='15'></PhotoBox>
             <PhotoBox imagem={aguia} nome=' Águia' nomecien=' Haliaeetus leucocephalus' classe=' Ave' anos='25'></PhotoBox>
             <PhotoBox imagem={papagaio} nome=' Arara' nomecien=' Ara chloropterus' classe=' Ave' anos='60'></PhotoBox>
-            <PhotoBox></PhotoBox>
-            <PhotoBox></PhotoBox>
+            <PhotoBox imagem={cardeal} nome=' Cardeal' nomecien=' Paroaria' classe=' Ave' anos='14'></PhotoBox>
+            <PhotoBox imagem={flamingo} nome=' Flamingo' nomecien=' Phoenicopterus' classe=' Ave' anos='50'></PhotoBox>
             <PhotoBox></PhotoBox>
             <PhotoBox></PhotoBox>
             <PhotoBox></PhotoBox>
