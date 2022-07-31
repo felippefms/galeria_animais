81add24463c879c9ee719ce271de91d67bf95082
Felipe Madureira de Souza <Felippefms3@gmail.com>
2022-07-31 12:10:43 -0300

    update

--- src/components/Terrestres.js
+++ src/components/Terrestres.js
@@ -9,6 +9,8 @@ import zebra from '../imgs/zebra.jpg';
 import elefante from '../imgs/elefante.jpg';
 import urso from '../imgs/urso.jpg';
 import iguana from '../imgs/iguana.jpg';
+import cavalo from '../imgs/cavalo.jpg';
+import macaco from '../imgs/macaco.jpg';
 
 import './terrestres.css';
 import Header from './Header';
@@ -24,8 +26,8 @@ function Terrestres(){
             <PhotoBox imagem={elefante} nome=' Elefante' nomecien=' Loxodonta' classe=' Mamifero' anos='60'></PhotoBox>
             <PhotoBox imagem={urso} nome=' Urso' nomecien=' Ursidae' classe=' Mamifero' anos='25'></PhotoBox>
             <PhotoBox imagem={iguana} nome=' Iguana' nomecien=' Iguana' classe=' Mamifero' anos='15'></PhotoBox>
-            <PhotoBox></PhotoBox>
-            <PhotoBox></PhotoBox>
+            <PhotoBox imagem={cavalo} nome=' Cavalo' nomecien=' Equus caballus' classe=' Mamifero' anos='30'></PhotoBox>
+            <PhotoBox imagem={macaco} nome=' Sagui Cara Branca' nomecien=' Callithrix geoffroyi' classe=' Mamifero' anos='12'></PhotoBox>
             <PhotoBox></PhotoBox>
             <PhotoBox></PhotoBox>
             <PhotoBox></PhotoBox>
