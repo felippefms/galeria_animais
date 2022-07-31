d401811a0f7a5aedf2241bdc49611cefac694ca1
Felipe Madureira de Souza <Felippefms3@gmail.com>
2022-07-31 01:02:04 -0300

    update

--- src/components/Voadores.js
+++ src/components/Voadores.js
@@ -14,6 +14,11 @@ import bluejay from '../imgs/bluejay.jpg';
 import coruja from '../imgs/coruja.jpg';
 import cardeal from '../imgs/cardinal.jpg';
 import flamingo from '../imgs/flamingo.jpg';
+import pato from '../imgs/pato.jpg';
+import periquito from '../imgs/periquito.jpg';
+import sula from '../imgs/sula.jpg';
+import picapau from '../imgs/picapau.jpg';
+import saira from '../imgs/saira.jpg';
 
 function Voadores(){
     return(
@@ -31,11 +36,11 @@ function Voadores(){
             <PhotoBox imagem={papagaio} nome=' Arara' nomecien=' Ara chloropterus' classe=' Ave' anos='60'></PhotoBox>
             <PhotoBox imagem={cardeal} nome=' Cardeal' nomecien=' Paroaria' classe=' Ave' anos='14'></PhotoBox>
             <PhotoBox imagem={flamingo} nome=' Flamingo' nomecien=' Phoenicopterus' classe=' Ave' anos='50'></PhotoBox>
-            <PhotoBox></PhotoBox>
-            <PhotoBox></PhotoBox>
-            <PhotoBox></PhotoBox>
-            <PhotoBox></PhotoBox>
-            <PhotoBox></PhotoBox>
+            <PhotoBox imagem={pato} nome=' Pato' nomecien=' Anatidae' classe=' Ave' anos='8'></PhotoBox>
+            <PhotoBox imagem={periquito} nome=' Periquito' nomecien=' Melopsittacus undulatus' classe=' Ave' anos='7'></PhotoBox>
+            <PhotoBox imagem={sula} nome=' Sula patiazul' nomecien=' Sula nebouxii' classe=' Ave' anos='17'></PhotoBox>
+            <PhotoBox imagem={picapau} nome=' Pica-Pau' nomecien=' Picidae' classe=' Ave' anos='15'></PhotoBox>
+            <PhotoBox imagem={saira} nome=' Sairá-Sete-Cores' nomecien=' Tangara seledon' classe=' Ave' anos='10'></PhotoBox>
         </div>
     </div>
     )
