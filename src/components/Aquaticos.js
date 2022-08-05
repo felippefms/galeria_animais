615aa7caf16e6a0278948f1d4cd6773e11c5197b
Felipe Madureira de Souza <Felippefms3@gmail.com>
2022-08-04 23:33:28 -0300

    Update

--- src/components/Aquaticos.js
+++ src/components/Aquaticos.js
@@ -4,29 +4,32 @@ import Header from './Header';
 
 import './Aquaticos.css';
 
-import golfinho from '../imgs/golfinho.jpg'
+import baleia from '../imgs/baleia.jpg';
+import golfinho from '../imgs/golfinho.jpg';
+import polvo from '../imgs/polvo.jpg';
+import carangueijo from '../imgs/carangueijo.jpg';
 
 function Aquaticos(){
     return(
         <div id='AquaticosWrapper'>
             <Header/>
             <div className='content-wrapper'>
-                <PhotoBox></PhotoBox>
-                <PhotoBox></PhotoBox>
-                <PhotoBox></PhotoBox>
-                <PhotoBox></PhotoBox>
-                <PhotoBox></PhotoBox>
+                <PhotoBox imagem={baleia} nome=' Baleia Orca' nomecien=' Orcinus orca' classe=' Mamifero' anos='40'></PhotoBox>
+                <PhotoBox imagem={polvo} nome=' Polvo' nomecien=' Octopoda' classe=' Molusco' anos='4'></PhotoBox>
+                <PhotoBox imagem={carangueijo} nome=' Carangueijo' nomecien=' Brachyura' classe=' Crustáceo' anos='4'></PhotoBox>
+                <PhotoBox imagem={golfinho} nome=' Golfinho' nomecien=' Delphinus delphis' classe=' Mamifero' anos='30'></PhotoBox>
+                <PhotoBox imagem={golfinho} nome=' Golfinho' nomecien=' Delphinus delphis' classe=' Mamifero' anos='30'></PhotoBox>
                 <PhotoBox></PhotoBox>
                 <PhotoBox imagem={golfinho} nome=' Golfinho' nomecien=' Delphinus delphis' classe=' Mamifero' anos='30'></PhotoBox>
                 <PhotoBox></PhotoBox>
-                <PhotoBox></PhotoBox>
-                <PhotoBox></PhotoBox>
-                <PhotoBox></PhotoBox>
-                <PhotoBox></PhotoBox>
-                <PhotoBox></PhotoBox>
-                <PhotoBox></PhotoBox>
-                <PhotoBox></PhotoBox>
-                <PhotoBox></PhotoBox>
+                <PhotoBox imagem={golfinho} nome=' Golfinho' nomecien=' Delphinus delphis' classe=' Mamifero' anos='30'></PhotoBox>
+                <PhotoBox imagem={golfinho} nome=' Golfinho' nomecien=' Delphinus delphis' classe=' Mamifero' anos='30'></PhotoBox>
+                <PhotoBox imagem={golfinho} nome=' Golfinho' nomecien=' Delphinus delphis' classe=' Mamifero' anos='30'></PhotoBox>
+                <PhotoBox imagem={golfinho} nome=' Golfinho' nomecien=' Delphinus delphis' classe=' Mamifero' anos='30'></PhotoBox>
+                <PhotoBox imagem={golfinho} nome=' Golfinho' nomecien=' Delphinus delphis' classe=' Mamifero' anos='30'></PhotoBox>
+                <PhotoBox imagem={golfinho} nome=' Golfinho' nomecien=' Delphinus delphis' classe=' Mamifero' anos='30'></PhotoBox>
+                <PhotoBox imagem={golfinho} nome=' Golfinho' nomecien=' Delphinus delphis' classe=' Mamifero' anos='30'></PhotoBox>
+                <PhotoBox imagem={golfinho} nome=' Golfinho' nomecien=' Delphinus delphis' classe=' Mamifero' anos='30'></PhotoBox>
             </div>
         </div>
     )
